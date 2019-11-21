50d16cfac8123bf983a538517921a188e24d042b
Tariq Bailey <tariqbailey627@gmail.com>
2019-11-21 02:21:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,14 +6,14 @@ require 'nokogiri'
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-def create_project_hash
-  projects = {}
-kickstarter.css("li.project.grid_4").each do |project|
-  title = project.css("h2.bbcard_name strong a").text
-  projects[title.to_sym] = {}
-end
-
-  
+  def create_project_hash
+    projects = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+    end
+  projects 
+  end
 end
 
 create_project_hash
