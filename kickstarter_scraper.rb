6f95fa3a66ecec16c976205387f6ccdc69ded930
Tariq Bailey <tariqbailey627@gmail.com>
2019-11-21 02:11:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
 end
 
 create_project_hash
